964d76fedbafa62afc2fdf1ebd34d6dab7a3bf1b
Travis Fischer <fisch0920@gmail.com>
2019-08-09 16:08:33 -0400

    📧

--- components/PricingSection/PricingSection.js
+++ components/PricingSection/PricingSection.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import { Divider, Typography } from 'antd'
+import cs from 'classnames'
 
 import { FinContext } from '../FinContext'
+import { CTAButton } from '../CTAButton'
+import { Paper } from '../Paper'
+
+import infinity from '../../assets/infinity.svg'
 
 import styles from './styles.module.css'
+
+const { Text } = Typography
 
 export class PricingSection extends Component {
   render() {
@@ -18,6 +26,64 @@ export class PricingSection extends Component {
 
               <div className={styles.subtitle}>
                 Free. Unlimited. <b>Pay as you Grow.</b>
+              </div>
+
+              <div className={styles.plans}>
+                <Paper className={styles.plan}>
+                  <h3 className={styles.name}>
+                    Free
+                  </h3>
+
+                  <Divider />
+
+                  <div className={cs(styles.feature, styles.split)}>
+                    <span>
+                      $0 / call
+                    </span>
+
+                    <span>
+                      N / day
+                    </span>
+                  </div>
+
+                  <Divider />
+
+                  <div className={cs(styles.feature, styles.split)}>
+                    <span>
+                      $0
+                    </span>
+
+                    <span>
+                      <img src={infinity} style={{ height: '1.5em' }} />
+                    </span>
+                  </div>
+
+                  <Divider />
+
+                  <div className={cs(styles.feature, styles.split)}>
+                    <span>
+                      $0 / GB
+                    </span>
+
+                    <span>
+                      100 GB / mo
+                    </span>
+                  </div>
+
+                  <Divider />
+
+                  <Text type='primary'>
+                    FREE FOREVER
+                  </Text>
+
+                  <div className={styles.price}>
+                    $0.00 / mo
+                  </div>
+
+                  <CTAButton type='secondary' inline>
+                    Get Started
+                  </CTAButton>
+                </Paper>
               </div>
             </div>
           </section>
