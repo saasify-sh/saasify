df081bb4037dabd6ec375ec4cdafdef23d9f777f
Travis Fischer <fisch0920@gmail.com>
2019-08-19 21:27:36 -0400

    🔀

--- components/FeaturesSection/FeaturesSection.js
+++ components/FeaturesSection/FeaturesSection.js
@@ -1,45 +1,41 @@
 import React, { Component } from 'react'
 
-import { FinContext } from '../FinContext'
 import { Section } from '../Section'
 import { UndrawSVG } from '../UndrawSVG'
 
+import features from './features.json'
 import styles from './styles.module.css'
 
 export class FeaturesSection extends Component {
   render() {
     return (
-      <FinContext.Consumer>
-        {project => (
-          <Section
-            title='Features'
-            stretch
-            {...this.props}
-          >
-            <div className={styles.features}>
-              {project.saas.features.map((feature) => (
-                <div
-                  className={styles.feature}
-                  key={feature.title}
-                >
-                  <UndrawSVG
-                    name={feature.undrawSVG}
-                    className={styles.illustration}
-                  />
+      <Section
+        title='Features'
+        stretch
+        {...this.props}
+      >
+        <div className={styles.features}>
+          {features.map((feature) => (
+            <div
+              className={styles.feature}
+              key={feature.title}
+            >
+              <UndrawSVG
+                name={feature.undrawSVG}
+                className={styles.illustration}
+              />
 
-                  <h3>
-                    {feature.title}
-                  </h3>
+              <h3>
+                {feature.title}
+              </h3>
 
-                  <p>
-                    {feature.desc}
-                  </p>
-                </div>
-              ))}
+              <p>
+                {feature.desc}
+              </p>
             </div>
-          </Section>
-        )}
-      </FinContext.Consumer>
+          ))}
+        </div>
+      </Section>
     )
   }
 }
