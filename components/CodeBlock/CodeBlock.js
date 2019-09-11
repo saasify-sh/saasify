68d2c98aafdecb0b35c1202eaf9c6b9f3e17a851
Travis Fischer <fisch0920@gmail.com>
2019-09-10 23:07:28 -0400

    😆

--- components/CodeBlock/CodeBlock.js
+++ components/CodeBlock/CodeBlock.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import theme from 'lib/theme'
 
-import SyntaxHighlighter from 'react-syntax-highlighter/prism'
-import codeTheme from 'react-syntax-highlighter/styles/prism/prism'
+import { SyntaxHighlighter, theme as codeTheme } from 'lib/react-syntax-highlighter'
 
 import styles from './styles.module.css'
 
