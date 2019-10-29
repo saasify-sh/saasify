5e8a23120fb22a71173058dc3fae7ec18306f7d3
Chris Villa <chrisvilla@me.com>
2019-10-29 20:16:50 +0000

    feat: allow user background image in waves theme

--- packages/react-saasify/src/themes/waves/index.js
+++ packages/react-saasify/src/themes/waves/index.js
@@ -1,5 +1,7 @@
 import styles from './waves.module.css'
 import cv from 'css-var'
+import waveSvg from './assets/wave.svg'
+import waveShadowedSvg from './assets/wave-shadowed.svg'
 
 export const waves = ({
   backgroundImage,
@@ -10,7 +12,18 @@ export const waves = ({
   // Define root CSS vars
   cv({
     'border-radius': buttonStyle === 'rounded' ? '100px' : '4px',
-    'color-primary': color
+    'color-primary': color,
+
+    // Adjust based on background image
+    'background-image': backgroundImage
+      ? `linear-gradient(180deg, rgba(158,143,143,0.50) 0%, rgba(0,0,0,0.50) 100%), url(${backgroundImage})`
+      : 'linear-gradient(104deg, #fcfcfc 13%, #f5f7fb 91%)',
+    'background-image-wave': backgroundImage
+      ? `url(${waveSvg})`
+      : `url(${waveShadowedSvg})`,
+    'color-nav-secondary-cta': backgroundImage ? 'white' : color,
+    'hero-color': backgroundImage ? 'white' : '#3a3a3a',
+    'nav-border-width': backgroundImage ? '0px' : '1px'
   })
 
   // TODO: make styles more dynamic based on less variables
