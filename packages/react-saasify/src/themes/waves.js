6cdde94a5fe84110ece69fb9eb8560f84e74369e
Chris Villa <chrisvilla@me.com>
2019-10-29 18:11:54 +0000

    feat: allow user button style in waves theme

--- packages/react-saasify/src/themes/waves.js
+++ packages/react-saasify/src/themes/waves.js
@@ -1,9 +1,17 @@
 import styles from './waves.module.css'
 import cv from 'css-var'
 
-export const waves = ({ color = '#5061CB', ...opts } = {}) => {
+export const waves = ({
+  backgroundImage,
+  buttonStyle = 'normal',
+  color = '#5061CB',
+  ...opts
+} = {}) => {
   // Define root CSS vars
-  cv({ 'color-primary': color })
+  cv({
+    'border-radius': buttonStyle === 'rounded' ? '100px' : '4px',
+    'color-primary': color
+  })
 
   // TODO: make styles more dynamic based on less variables
   // TODO: add fonts
