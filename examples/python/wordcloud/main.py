0e606f510f9c969eb1aff660624c5344c5c94215
Travis Fischer <fisch0920@gmail.com>
2019-11-09 19:37:29 -0500

    format python code with black

--- examples/python/wordcloud/main.py
+++ examples/python/wordcloud/main.py
@@ -10,13 +10,14 @@ from stylecloud import stylecloud as sc
 from typing import Optional
 from wordcloud import WordCloud
 
+
 class StyleCloudRequest(BaseModel):
     url: str = None
     text: str = None
     size: int = 512
-    icon_name: str = 'fas fa-flag'
-    palette: str = 'cartocolors.qualitative.Bold_6'
-    background_color: str = 'white'
+    icon_name: str = "fas fa-flag"
+    palette: str = "cartocolors.qualitative.Bold_6"
+    background_color: str = "white"
     max_font_size: int = 200
     max_words: int = 2000
     stopwords: bool = True
@@ -31,27 +32,27 @@ class WordCloudRequest(BaseModel):
     margin: int = 2
     ranks_only: bool = None
     prefer_horizontal: float = 0.9
-    #mask: = None
+    # mask: = None
     scale: float = 1
-    #color_func = None
+    # color_func = None
     max_words: int = 200
     min_font_size: int = 4
-    #stopwords = None
+    # stopwords = None
     random_state: str = None
-    background_color: str = 'black'
+    background_color: str = "black"
     max_font_size: Optional[int] = None
     font_step: float = 1
-    mode: str = 'RGB'
-    relative_scaling: str = 'auto'
-    #regexp = None
+    mode: str = "RGB"
+    relative_scaling: str = "auto"
+    # regexp = None
     collocations: bool = True
-    #colormap = None
+    # colormap = None
     normalize_plurals: bool = True
     contour_width: float = 0
-    contour_color: str = 'black'
+    contour_color: str = "black"
     repeat: bool = False
-    #include_numbers: bool = False
-    #min_word_length: int = 0
+    # include_numbers: bool = False
+    # min_word_length: int = 0
 
 
 load_dotenv()
@@ -60,69 +61,67 @@ app = FastAPI()
 
 # ensure that all result images are cached aggressively
 headers = {
-    'Cache-Control': 'public, immutable, no-transform, s-maxage=31536000, max-age=31536000'
+    "Cache-Control": "public, immutable, no-transform, s-maxage=31536000, max-age=31536000"
 }
 
 responses = {
     200: {
-        'content': {'image/png': {}},
-        'description': 'Return a stylecloud image of the input text.',
+        "content": {"image/png": {}},
+        "description": "Return a stylecloud image of the input text.",
     }
 }
 
-DIFFBOT_TOKEN = os.getenv('DIFFBOT_TOKEN')
+DIFFBOT_TOKEN = os.getenv("DIFFBOT_TOKEN")
+
 
 def require_diffbot():
     if DIFFBOT_TOKEN is None:
-        raise Exception('url support requires a valid diffbot token')
+        raise Exception("url support requires a valid diffbot token")
 
 
 @app.post(
-    '/stylecloud',
+    "/stylecloud",
     responses={
         200: {
-            'content': {'image/png': {}},
-            'description': 'Return a stylecloud image of the input text.',
+            "content": {"image/png": {}},
+            "description": "Return a stylecloud image of the input text.",
         }
-    }
+    },
 )
 def stylecloud(request: StyleCloudRequest):
     params = request.dict()
-    url = params.pop('url', None)
-    text = params.pop('text', None)
+    url = params.pop("url", None)
+    text = params.pop("text", None)
 
     if url is not None:
         require_diffbot()
-        article = diffbot.article(url, token=DIFFBOT_TOKEN)['objects'][0]
+        article = diffbot.article(url, token=DIFFBOT_TOKEN)["objects"][0]
         pprint.pprint(article)
-        text = article['text']
+        text = article["text"]
     elif text is None:
         raise Exception('Must provide either"text" or "url".')
 
     sc.gen_stylecloud(**params, text=text)
 
-    return FileResponse('stylecloud.png', media_type='image/png', headers=headers)
+    return FileResponse("stylecloud.png", media_type="image/png", headers=headers)
 
 
-@app.post(
-    '/wordcloud',
-    responses=responses
-)
+@app.post("/wordcloud", responses=responses)
 def wordcloud(request: WordCloudRequest):
     params = request.dict()
-    url = params.pop('url', None)
-    text = params.pop('text', None)
+    url = params.pop("url", None)
+    text = params.pop("text", None)
 
     if url is not None:
         require_diffbot()
         article = diffbot.article(url, token=DIFFBOT_TOKEN).objects[0]
         pprint.pprint(article)
-        text = article['text']
+        text = article["text"]
     elif text is None:
         raise Exception('Must provide either"text" or "url".')
 
     wc = WordCloud(**params)
     wc.generate_from_text(text)
-    wc.to_file('wordcloud.png')
+    wc.to_file("wordcloud.png")
 
-    return FileResponse('wordcloud.png', media_type='image/png', headers=headers)
+    return FileResponse("wordcloud.png", media_type="image/png", headers=headers)
