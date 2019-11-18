e2c0890bf9e7a78abd78c537d3f5d60dd23e456d
Travis Fischer <fisch0920@gmail.com>
2019-11-18 01:40:46 -0500

    feat: add smart fuzzy search to demo enums

--- packages/react-saasify/src/components/ServiceForm/ServiceInputSelect.js
+++ packages/react-saasify/src/components/ServiceForm/ServiceInputSelect.js
@@ -1,28 +1,60 @@
-import React, { useCallback } from 'react'
+import React, { Component } from 'react'
 import { ServiceInputWrapperWithLabel } from './ServiceInputWrapperWithLabel'
 import { AutoComplete } from 'lib/antd'
+import Fuse from 'fuse.js'
 
-export const ServiceInputSelect = ({
-  default: defaultValue,
-  onChange,
-  enum: items,
-  ...props
-}) => {
-  const onChangeMem = useCallback(
-    (value) => onChange({ target: { value, name: props.propKey } }),
-    []
-  )
+const style = { width: '100%' }
 
-  return (
-    <ServiceInputWrapperWithLabel {...props}>
-      <AutoComplete
-        dataSource={items}
-        defaultValue={defaultValue}
-        style={{ width: '100%' }}
-        onSelect={onChangeMem}
-        filterOption
-        placeholder='Select'
-      />
-    </ServiceInputWrapperWithLabel>
-  )
+export class ServiceInputSelect extends Component {
+  state = {
+    items: []
+  }
+
+  componentDidMount() {
+    const { props } = this
+    const docs = props.enum.map((item) => ({ n: item }))
+    this._fuse = new Fuse(docs, {
+      keys: ['n'],
+      location: 7,
+      threshold: 0.2,
+      shouldSort: true
+    })
+  }
+
+  render() {
+    const {
+      default: defaultValue,
+      onChange,
+      enum: enums,
+      ...props
+    } = this.props
+
+    const { items } = this.state
+    const children = items.map((item) => (
+      <AutoComplete.Option key={item.n}>{item.n}</AutoComplete.Option>
+    ))
+
+    return (
+      <ServiceInputWrapperWithLabel {...props}>
+        <AutoComplete
+          defaultValue={defaultValue}
+          style={style}
+          onSelect={this._onChange}
+          onSearch={this._onSearch}
+          placeholder='Select'
+        >
+          {children}
+        </AutoComplete>
+      </ServiceInputWrapperWithLabel>
+    )
+  }
+
+  _onChange = (value) => {
+    this.props.onChange({ target: { value, name: this.props.propKey } })
+  }
+
+  _onSearch = (value) => {
+    const results = this._fuse.search(value).slice(0, 32)
+    this.setState({ items: results })
+  }
 }
