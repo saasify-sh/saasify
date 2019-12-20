28e412fef0928e54f024755ce24f8f89ea6d12dd
Travis Fischer <fisch0920@gmail.com>
2019-12-19 23:19:58 -0500

    🥑

--- packages/saasify-faas-utils/index.js
+++ packages/saasify-faas-utils/index.js
@@ -3,8 +3,3 @@
 exports.parseFaasIdentifier = require('./lib/parse-faas-identifier')
 exports.parseFaasUri = require('./lib/parse-faas-uri')
 exports.validators = require('./lib/validators')
-exports.prepareDeployment = require('./lib/prepare-deployment')
-
-exports.parseOpenAPI = require('./lib/parse-openapi')
-exports.annotateOpenAPI = require('./lib/annotate-openapi')
-exports.convertOpenAPIToServices = require('./lib/convert-openapi-to-services')
