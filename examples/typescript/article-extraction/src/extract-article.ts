feccd4d25de32c09d31ad7ce2666f1d6a127dac1
Travis Fischer <fisch0920@gmail.com>
2020-01-06 04:21:44 -0500

    👥

--- examples/typescript/article-extraction/src/extract-article.ts
+++ examples/typescript/article-extraction/src/extract-article.ts
@@ -1,18 +1,6 @@
-import { HttpContext } from 'fts-http'
-
 import { Article } from './types'
 import extractArticleImpl from '../lib/extract-article'
 
-export default async function extractArticle(
-  url: string,
-  context?: HttpContext
-): Promise<Article> {
-  if (context) {
-    context.set(
-      'Cache-Control',
-      `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`
-    )
-  }
-
+export default async function extractArticle(url: string): Promise<Article> {
   return extractArticleImpl(url)
 }
