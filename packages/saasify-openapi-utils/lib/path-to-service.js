f86b51ec5e774a3b6a8d89d4613a10dc40618aed
Travis Fischer <fisch0920@gmail.com>
2020-02-06 03:06:50 -0500

    fix: small fix for fts openapis

--- packages/saasify-openapi-utils/lib/path-to-service.js
+++ packages/saasify-openapi-utils/lib/path-to-service.js
@@ -1,9 +1,18 @@
 'use strict'
 
+const { parseFaasIdentifier } = require('saasify-faas-utils')
+
 /**
  * Finds the service that corresponds with a particular relative URL path.
  */
 module.exports = (path, deployment) => {
+  if (path.includes('@')) {
+    const parsedPath = parseFaasIdentifier(path)
+    if (parsedPath) {
+      path = parsedPath.servicePath
+    }
+  }
+
   for (const service of deployment.services) {
     if (path === service.path) {
       return service
