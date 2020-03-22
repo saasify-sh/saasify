2b1f5fe568c768c88254eed4537e250b4aecbbce
Travis Fischer <fisch0920@gmail.com>
2020-03-22 15:58:44 -0400

    🐥

--- packages/saasify-sdk/test.js
+++ packages/saasify-sdk/test.js
@@ -6,19 +6,11 @@ const SaasifySDK = require('.')
 
 test('basic', async (t) => {
   const sdk = new SaasifySDK({
-    defaults: {
-      project: 'foo',
-      deployment: 'bar',
-      consumer: {
-        token: 'test-token'
-      }
-    }
+    developmentToken: 'test-token'
   })
 
   await sdk.ready
 
-  t.is(sdk.project, 'foo')
-  t.is(sdk.deployment, 'bar')
-  t.is(sdk.consumer.token, 'test-token')
+  t.is(sdk.token, 'test-token')
   t.is(sdk.api.token, 'test-token')
 })
